5e7036ea97c4df5841a684ba8972d006f12f4bb7
ruslan <rteimurov211@gmail.comgit config --global user.email rteimurov211@gmail.com>
2022-10-21 09:55:32 -0400

    Tank creation

---
+++ Tank.java
@@ -0,0 +1,17 @@
+class Main {
+
+    public static void main(String[] args) {
+
+
+        Tank tank = new Tank();
+        tank.goForward(10);
+        tank.printPosition();
+        //  tank.turnRight();
+        tank.goForward(50);
+        tank.printPosition();
+        //  tank.turnLeft();
+        tank.goBackward(100);
+        tank.printPosition();
+
+    }
+}
